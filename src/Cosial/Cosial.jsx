f4ee2a6c8f2d3d92b8b1cc0bd5584a64ef1342b2
viktr 666 <viktr666@gmail.com>
2021-11-27 11:01:15 +0300

    cg

--- src/Cosial/Cosial.jsx
+++ src/Cosial/Cosial.jsx
@@ -6,23 +6,23 @@ const Cosial = () => {
 
     return (
         <div className={clases.Cosial}>
-            
+
             <div className="Cosialicons">
                 <a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Vk_Logo.svg/1024px-Vk_Logo.svg.png" alt="" width='25px' /></a>
-            </div>       
+            </div>
 
             <div className="Cosialicons">
                 <a href="#"> <img src="https://mca-partner.ru/wp-content/uploads/2020/05/whatsapp-logo.png" alt="" width='25px' /></a>
-            </div>     
-            
-            <div className="Cosialicons">
-                <a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" alt="" width='25px' /></a>
-            </div>      
-            
             </div>
 
-            
-        
+            <div className="Cosialicons">
+                <a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" alt="" width='25px' /></a>
+            </div>
+
+        </div>
+
+        // zsfaf
+
     );
-    }
+}
 export default Cosial;
